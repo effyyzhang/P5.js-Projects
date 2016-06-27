08d2d41f7fb312cacc1eb961b8f661fc85971160
Effy Zhang <effy@squareup.com>
2016-06-27 13:20:23 -0700

    add video project

--- Sound Sythesis/sketch.js
+++ Sound Sythesis/sketch.js
@@ -1,17 +1,27 @@
+
+//tone.js 
+//traeger attact / release
+
 var wave;
 var slider;
 var button;
 var playing = false;
+var env;
 
 function setup(){
 	createCanvas(200,200);
 	background(250);
+
+	env = new p5.Env();
+	env.setADSR(0.5,0.25,0.5,0.1);
+	env.setRange(0.8,0);
+
 	wave = new p5.Oscillator();
 	slider = createSlider(100,1200,440);
 	
 	wave.setType('sine');
 	wave.start();
-	wave.amp(0);
+	wave.amp(env);
 	wave.freq(1000);
 
 
---
+++ Video_Photobooth/sketch.js
@@ -0,0 +1,45 @@
+
+var video;
+var button;
+var snapshots=[];
+var counter = 0;
+var go = false;
+
+function setup() {
+  createCanvas(640, 480);
+  background(51);
+  video = createCapture(VIDEO, ready);
+  video.size(640, 480);
+}
+
+
+function ready(){
+  go = true;
+}
+
+function draw() {
+  if (go){
+    snapshots[counter] = video.get();
+    counter ++;
+    if (counter == 71){
+      counter = 0;
+    };
+  }
+  var w = 80;
+  var h = 60;
+  var x = 0;
+  var y = 0;
+  for (var i = 0; i < snapshots.length; i++){
+    var index = (i+frameCount)%snapshots.length;
+    image(snapshots[i],x,y,w,h);
+    x = x + w;
+    if(x> width){
+      x = 0;
+      y = y+h;
+    }
+  }
+
+
+}
+
+
